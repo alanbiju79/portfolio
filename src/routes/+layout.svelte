fa6e25f12828d08c72898ecf1d9220513aa2845f
Alan Biju <alanbiju79@gmail.com>
2023-08-13 21:02:09 +0100

    add tailwind, add pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../app.css";
+  import Menu from "./Menu.svelte";
+</script>
+
+<div>
+	<Menu />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<!-- <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p> -->
+	</footer>
+</div>
---
+++ src/routes/Menu.svelte
@@ -0,0 +1,7 @@
+<div class="flex justify-center">
+  <ul class="menu menu-horizontal bg-base-200 rounded-box gap-1">
+    <li><a href="/" class="active">Home</a></li>
+    <li><a href="/projects">Projects</a></li>
+    <li><a href="/stack">Stack</a></li>
+  </ul>
+</div>
