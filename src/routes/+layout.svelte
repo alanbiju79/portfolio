bcacfd7237a72daaa697f384f1bbb1924bd22128
Alan Biju <alanbiju79@gmail.com>
2023-08-21 01:30:00 +0100

    add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,22 @@
   import "../app.css";
   import Menu from "./Menu.svelte";
 	import Footer from "./Footer.svelte";
+
+	import { fade } from 'svelte/transition'
+	export let data
 </script>
 
-<div class="w-full max-w-7xl bg-base-100 py-8 px-4 sm:px-24 border border-base-200 min-h-screen">
-	<Menu />
+{#key data.url}
+	<div class="w-full max-w-7xl bg-base-100 py-8 px-4 sm:px-24 border border-base-200 min-h-screen">
+		<Menu />
 
-	<main class="mt-16 sm:mt-20">
-		<slot />
-	</main>
+		<main class="mt-16 sm:mt-20"
+			in:fade={{duration: 200, delay: 200}}
+			out:fade={{duration: 200}}
+		>
+			<slot />
+		</main>
 
-	<Footer />
-		
-</div>
+		<Footer />	
+	</div>
+{/key}
